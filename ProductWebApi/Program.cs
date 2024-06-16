b54bb06818addccc2315349b444554e7ae339939
vmman <vmman@AAVN-LAPTOP215>
2024-06-16 11:24:28 +0700

    Add project files.

---
+++ ProductWebApi/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+//using JwtAuthenticationManager;
+using ProductWebApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+//builder.Services.AddCustomJwtAuthentication();
+
+/* Database Context Dependency Injection */
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword};TrustServerCertificate=True";
+builder.Services.AddDbContext<ProductDbContext>(opt => opt.UseSqlServer(connectionString));
+/* ===================================== */
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
